f4a3bbd6ed0cdd6074b44e5b08d3a1b957977c80
yus-ito <yus-ito@virtualex.co.jp>
2020-05-20 14:13:21 +0900

    蔵書登録作業途中

--- WebContent/manager/managerAdd.js
+++ WebContent/manager/managerAdd.js
@@ -1,3 +1,77 @@
-/**
- * 
- */
+function add() {
+
+	var jsid = $('#js-input-id').val()
+	var jstitle = $('#js-input-title').val()
+	var jsauthor = $('#js-input-author').val()
+	var jsgenre = $('genre').val()
+	var jsbuyer = $('#js-input-buyer').val()
+
+//現在の時刻から入力するファンクションを利用したいが現在実装できないので手入力
+
+	var jspurchaseDate = $('#js-input-purchasedate').val()
+
+//日付表示
+	window.onload = function () {
+    var today = new Date();
+    today.setDate(today.getDate());
+    var yyyy = today.getFullYear();
+    var mm = ("0"+(today.getMonth()+1)).slice(-2);
+    var dd = ("0"+today.getDate()).slice(-2);
+    document.getElementById("js-today").value=yyyy+'/'+mm+'/'+dd;　//IDはjs-todayとしてます。
+}
+
+	var requestQuery = {
+		id : jsid,
+		purchaseDate : jspurchaseDate,
+		title : jstitle,
+		author : jsauthor,
+		genre : jsgenre,
+		buyer : jsbuyer
+	}
+	// コンソールでrequestQueryを確認
+	console.log(requestQuery)
+	$.ajax({
+		type : "POST",
+		url : "http://localhost:8080/bookManagement/ManagerAddServlet",
+		data : requestQuery,
+		datatype : 'json',
+		success : function(json) {
+
+			// サーバーとの通信に成功した時の処理
+			// 確認のために返却値を出力
+			console.log('返却値', json);
+			// 登録完了のアラート
+			alert('登録が完了しました');
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			// サーバーとの通信に失敗したときの処理
+			alert('データの通信に失敗しました');
+			console.log(errorThrown)
+		}
+
+	});
+
+}
+
+//function getTodaysDate() {
+//
+//	var today = new Date();
+//
+//	var todaydate;
+//
+//	var Year = today.getFullYear();
+//	var Month = today.getMonth() + 1;
+//	var Date = today.getDate();
+//
+//	todaydate = Year + "-" + Month + "-" + Date;
+//
+//	console.log(purcgaseDate);
+//
+//	return todaydate;
+//}
+//日付表示
+
+
+$(document).ready(function() {
+	$('#js-add-button').click(add)
+});
