4f9c1198fe27ac9a5f954d9c291444f91e777cd9
yus-ito <yus-ito@virtualex.co.jp>
2020-05-20 14:17:42 +0900

    蔵書登録作業途中2

--- WebContent/manager/managerAdd.js
+++ WebContent/manager/managerAdd.js
@@ -10,15 +10,6 @@ function add() {
 
 	var jspurchaseDate = $('#js-input-purchasedate').val()
 
-//日付表示
-	window.onload = function () {
-    var today = new Date();
-    today.setDate(today.getDate());
-    var yyyy = today.getFullYear();
-    var mm = ("0"+(today.getMonth()+1)).slice(-2);
-    var dd = ("0"+today.getDate()).slice(-2);
-    document.getElementById("js-today").value=yyyy+'/'+mm+'/'+dd;　//IDはjs-todayとしてます。
-}
 
 	var requestQuery = {
 		id : jsid,
