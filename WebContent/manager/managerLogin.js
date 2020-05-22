6eefdbf036890254af62d77f2a83a2a108f8b548
yus-ito <yus-ito@virtualex.co.jp>
2020-05-22 17:24:10 +0900

    全画面認証完成（モンキーテスト中2）

--- WebContent/manager/managerLogin.js
+++ WebContent/manager/managerLogin.js
@@ -50,30 +50,6 @@ function login() {
 	});
 }
 
-//ログアウトボタン実装用に仮で設置、employeeSearchに[移動予定]
-
-function logoutAjax() {
-
-	$.ajax({
-		type : 'GET',
-		url : 'http://localhost:8080/bookManagement/ManegerLoginServlet',
-//		dataType : 'json',
-//		data : requestQuery,
-		success : function(json) {
-			// サーバーとの通信に成功した時の処理
-			// 確認のために返却値を出力
-			console.log('返却値', json);
-			window.location.href="http://localhost:8080/bookManagement/manager/managerLogin.html";
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			// サーバーとの通信に失敗した時の処理
-			alert('データの通信に失敗しました');
-			console.log(errorThrown)
-		}
-	});
-}
-
-
 /**
  * 読み込み時の動作
  */
@@ -81,9 +57,5 @@ $(document).ready(function() {
 
 	// ログインボタンを押したときのイベント
 	$('#js-login-button').click(login);
-
-	//ログアウトボタンを押したときのイベント[要移動]
-	$('#js-logout-button').click(logoutAjax);
-
 
 });
